569498d141097823b7da5ce51bfaf25f75492746
TheCivilIndia <thecivilindiakanpur@gmail.com>
2025-08-01 13:44:43 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -23,11 +23,32 @@ import KVSPrimaryTeacherPage from './pages/teaching/KVSPrimaryTeaching';
 import CTETUPTETPage from './pages/teaching/CTETPage';
 import UPSCExamPage from './pages/UPSCExam';
 import Footer from './components/Footer';
+import { Helmet, HelmetProvider } from 'react-helmet-async';
 
 
 
 function App() {
   return (
+    <HelmetProvider>
+     <Helmet>
+        {/* Primary Meta Tags */}
+        <title>The Civil India | Top Civil Services Coaching</title>
+        <meta name="description" content="India's leading institute for UPSC, PCS, SSC & banking exams. 20,000+ selections since 2006." />
+        
+        {/* Open Graph/Facebook */}
+        <meta property="og:type" content="website" />
+        <meta property="og:url" content="https://thecivilindia.in" />
+        <meta property="og:title" content="The Civil India | Premier Civil Services Coaching" />
+        <meta property="og:description" content="Transform your career with India's most successful civil services coaching institute." />
+        {/* <meta property="og:image" content="https://thecivilindia.in/og-image.jpg" /> */}
+
+        {/* Twitter */}
+        <meta name="twitter:card" content="summary_large_image" />
+        <meta name="twitter:site" content="@TheCivilIndia" />
+        <meta name="twitter:title" content="India's #1 Civil Services Coaching" />
+        <meta name="twitter:description" content="Join 20,000+ successful candidates with our proven strategies." />
+        {/* <meta name="twitter:image" content="https://thecivilindia.in/twitter-card.jpg" /> */}
+      </Helmet>
     <main className='w-[100vw] min-h-screen flex flex-col'>
       <Router>
         <Navbar />
@@ -55,6 +76,7 @@ function App() {
         <Footer/>
       </Router>
     </main>
+    </HelmetProvider>
   );
 }
 
